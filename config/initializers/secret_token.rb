3b603fd4de3eb6db2f2b60c82ddeaeb6a7777dbd
Samuel Heaney <sheaney@gmail.com>
2013-04-27 15:24:44 -0500

    Set up rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monkeytrip::Application.config.secret_token = '5a2f52b9f39345df51bbd84acddf8977c0b2853315b154a79d6122f305a5d7e385211cee1e8d1d9e31abdd4ef6be9655a733428544fe721c32c61950bcdc41a6'
